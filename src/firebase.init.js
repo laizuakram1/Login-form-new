85e835cf592ea2215ab59b0f312c16871add2c22
laizuakram1 <laizuakram98@gmail.com>
2022-04-09 23:28:13 +0600

    create navlink and routes

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzGmsbRnr8olZiFp1O2ILYmQfhYpA0XD0",
+  authDomain: "login-form-new-facea.firebaseapp.com",
+  projectId: "login-form-new-facea",
+  storageBucket: "login-form-new-facea.appspot.com",
+  messagingSenderId: "345632301764",
+  appId: "1:345632301764:web:05bb72003eec88a0599f91"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
